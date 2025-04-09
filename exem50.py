ac11d8d1dd6dba53cdee4d936503368b4ab96fcd
renegadelhaedu <renegadelha.edu@gmail.com>
2025-04-08 21:37:42 -0300

    aula 0804

--- exem50.py
+++ exem50.py
@@ -1,5 +1,6 @@
 usuarios = list()
 senhas = list()
+mensagens = []
 
 opcao = 99
 
@@ -13,9 +14,11 @@ while opcao != 0:
     if opcao == 1:
         login = input('qual o login do usuario')
         senha = input('qual a senha do usuario')
+
         if login not in usuarios:
             usuarios.append(login)
             senhas.append(senha)
+            mensagens.append('')
             print('usuario cadastrado com sucesso!')
         else:
             print('usuario existente no sistema')
@@ -28,7 +31,20 @@ while opcao != 0:
             indice = usuarios.index(login_user)
             if senha_user == senhas[indice]:
                 print('login realizado com sucesso')
-                print('---MENU usuario---')
+                opcao2 = 99
+                while opcao2 != 0:
+                    print('\n---MENU usuario---')
+                    print('1-inserir mensagem no diario')
+                    print('2-exibir mensagens')
+                    print('0-sair da conta')
+                    opcao2 = int(input('digite a opcao desejada'))
+                    if opcao2 == 1:
+                        mensagem = input('digite a mensagem')
+                        mensagens[indice] += mensagem + '\n'
+                    elif opcao2 == 2:
+                        print(f'Mensagens de {login_user}')
+                        print(mensagens[indice])
+
 
             else:
                 print('usuario ou senha incorretos')
