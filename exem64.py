770865230dfa4670ceb749991467ae1547ff882f
renegadelhaedu <renegadelha.edu@gmail.com>
2025-05-05 19:34:33 -0300

    aula 0505

---
+++ exem64.py
@@ -0,0 +1,11 @@
+#estrutura de dados: lista e dicionário
+#novas: tupla e conjunto
+#tupla ela é imutável
+
+people = [2,3,5]  #lista
+opcoes = ('casado','solteiro','divorciado','viuvo') #tupla
+
+if opcoes.count('cjjjjj') > 0:
+    print('existe na tupla')
+else:
+    print('NAO existe na tupla')
---
+++ exem65.py
@@ -0,0 +1,12 @@
+#estrutura de dados: lista e dicionário
+#novas: conjunto
+#conjunto = unicidade| exclusividade dos itens | nao se repetem
+pessoas = set()
+
+#dicionario
+pessoas1 = {'rene':36, 'andre':81}
+
+#set
+alunos = {'jose','maria','pedro','aline','maria'}
+
+alunos.add('Jose')
